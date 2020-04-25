769804b0ad0a5ccf25dd5d81af8f0dbbcd345721
Christian Teeples <36543358+cteeples@users.noreply.github.com>
2020-04-25 16:40:26 -0500

    changes 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import {Col, Row, Card, Container} from 'react-bootstrap';
 
 import {Layout} from './components/Layout';
